f7ef4ad85807f22f4221ad089175aebe6527ae24
Arnav-Singh2453 <1si24is12@sit.ac.in>
2026-09-23 21:15:29 +0530

    bugfix4

--- src/main/java/org/arnav/bankapp/functions/HomeController.java
+++ src/main/java/org/arnav/bankapp/functions/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
         @GetMapping("/")
         public String home() {
-            return "index.jsp"; // Maps to index.jsp
+            return "index"; // Maps to index.jsp
         }
 }
